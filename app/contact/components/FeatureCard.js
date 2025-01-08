558988c1e436a7b23ca885b567d4999df43a814b
Carlos Barreto <dev.carlos17@gmail.com>
2025-01-08 16:47:19 -0400

    FeatureCard componente edited

--- app/contact/components/FeatureCard.js
+++ app/contact/components/FeatureCard.js
@@ -1,6 +1,6 @@
 export default function FeatureCard({ title, description }) {
   return (
-    <div className="flex flex-col text-start tracking-wide h-full w-[60vw] md:w-[30vw] lg:w-[25vw] space-y-2">
+    <div className="flex flex-col text-start tracking-wide h-full w-[60vw] md:w-[30vw] lg:w-[25vw] xl:w-[18vw] space-y-2">
       <span className="text-center md:w-[100%] lg:w-full text-2xl md:text-lg  xl:text-2xl font-bold mb-2 bg-gradient-to-r from-black from-10% to-gray-700 p-2 rounded-xl">
         {title}
       </span>
