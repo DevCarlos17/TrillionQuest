c0099fa220d05a36205a99d34e7b958a913d9624
Carlos Barreto <dev.carlos17@gmail.com>
2025-01-08 13:57:58 -0400

    update Service Overview component

--- app/contact/sections/ServicesOverview.js
+++ app/contact/sections/ServicesOverview.js
@@ -4,9 +4,9 @@ import Button from '@/app/components/Button';
 
 export default function ServicesOverview() {
   return (
-    <div className="w-[90vw] h-[150vh] md:h-[110vh] lg:h-[100vh] xl:h-[120vh]  space-y-40 md:space-y-20 xl:space-y-20 flex flex-col justify-center md:justify-between items-center">
+    <div className="w-[90vw] min-h-screen sm:h-[135vh] md:h-[110vh] lg:h-[100vh] xl:h-[120vh]  space-y-20 sm:space-y-28 md:space-y-20 xl:space-y-20 flex flex-col justify-center md:justify-between items-center">
       {/*First Section*/}
-      <section className="flex flex-col md:flex-row w-full h-[30vh] md:h-[40vh] justify-center items-center xl:-space-x-20 space-y-9 md:space-y-0 ">
+      <section className="flex flex-col md:flex-row w-full h-[40%] md:h-[40vh] justify-center items-center xl:-space-x-20 space-y-9 md:space-y-0 ">
         <FeatureHighlight
           number="01"
           title="Strategic Branding"
@@ -24,7 +24,7 @@ export default function ServicesOverview() {
         />
       </section>
       {/*Second Section*/}
-      <section className="flex flex-col md:flex-row w-full h-[30vh] md:h-[30vh] justify-around items-center space-y-6 md:space-y-0 md:space-x-6 xl:-space-x-20">
+      <section className="flex flex-col md:flex-row w-full h-[40%] sm:h-[48vh] md:h-[30vh] justify-around items-center space-y-6 md:space-y-0 md:space-x-6 xl:-space-x-20">
         <FeatureCard
           title="MULTIDISCIPLINARY EXPERTISE"
           description="Our team of strategists, creatives, content specialists, SEO experts, digital advertisers, and analysts."
@@ -39,15 +39,15 @@ export default function ServicesOverview() {
         />
       </section>
       {/*Third Section*/}
-      <div className="w-full h-[40vh] md:h-[20vh] lg:h-[30vh] flex flex-col justify-center space-y-5 md:justify-evenly items-center md:space-y-2 ">
-        <span className="text-4xl  lg:text-3xl xl:text-6xl font-bold">
+      <div className="w-full h-[10%] sm:h-[8vh] md:h-[20vh] lg:h-[30vh] flex flex-col justify-center space-y-5 md:justify-evenly items-center md:space-y-2 ">
+        <span className="text-4xl  lg:text-3xl xl:text-5xl font-bold">
           Tell us, what do you need?
         </span>
 
         <Button
           name={'Contact Us'}
           style={
-            'text-lg w-[40%] h-[30%] sm:w-[30%] md:w-[30%] lg:w-[22%] xl:w-[18%] 2xl:w-[16%]  md:h-[30%]  2xl:text-3xl font-extrabold md:text-xl  tracking-widest  h-[7vh]  uppercase traacking-widest'
+            'text-lg w-[40%] h-[30%] sm:w-[30%] md:w-[30%] lg:w-[22%] xl:w-[18%] 2xl:w-[14%]  md:h-[25%]  2xl:text-2xl font-extrabold md:text-xl  tracking-widest  h-[7vh]  uppercase traacking-widest'
           }
         />
       </div>
