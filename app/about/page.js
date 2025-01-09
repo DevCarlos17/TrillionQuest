9777f21c698ef03c7e7608d61a8924b63cf3a5b9
Fernando Barrera <barrerafernando54@gmail.com>
2025-01-09 16:10:03 -0400

    About en mobile

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,12 @@
+import AboutText from "../components/AboutText";
+import Gallery from "../components/Gallery";
+
 export default function About() {
   return (
     <div className="min-h-screen w-full flex flex-col  items-center justify-center ">
       <h1 className="text-5xl font-bold mb-5">About Us</h1>
+      <AboutText />
+      {/* <Gallery /> */}
     </div>
   );
 }
